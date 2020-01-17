7eac56ab1fda63aec03b0443efcaf3595a5da950
Quan <urjhams@gmail.com>
2020-01-18 00:07:05 +0100

    update

--- Assets/Scenes/Sample/EyeBehaviourDemo.cs
+++ Assets/Scenes/Sample/EyeBehaviourDemo.cs
@@ -21,6 +21,33 @@ public class EyeBehaviourDemo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!Screen.fullScreen)
+        {
+            _text.text = "<color=red>Please run in full screen!</color>";
+            return;
+        }
+
+        // Quit if escape is pressed.
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!Application.isEditor)
+            {
+                Application.Quit();
+            }
+        }
+
+        // We are expecting to have all objects.
+        if (!_eyeTracker || !_calibration || !_trackBoxGuide)
+        {
+            return;
+        }
+
+        // Thin out updates a bit.
+        if (Time.frameCount % 6 != 0)
+        {
+            return;
+        }
+
         if (!_eyeTracker.LatestProcessedGazeData.Left.GazePointValid) { return; }
         var left = _eyeTracker.LatestProcessedGazeData.Left.GazePointOnDisplayArea;
         var right = _eyeTracker.LatestProcessedGazeData.Right.GazePointOnDisplayArea;
